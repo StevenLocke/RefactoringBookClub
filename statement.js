e131031c64f57c4ae72fd34298f5b1ed29ac3a0f
Steven Locke <slocke@pivotal.io>
2018-10-24 14:17:15 -0700

    Extracted switch

--- statement.js
+++ statement.js
@@ -24,6 +24,33 @@ const plays = {
 };
 
 
+const calculateAmountBasedonPlayGenreAndAudience = (play, perf) => {
+  let x = 0;
+  switch (play.type) {
+    case "tragedy":
+      x = 40000;
+      if (perf.audience > 30) {
+        x += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      x = 30000;
+      if (perf.audience > 20) {
+        x += 10000 + 500 * (perf.audience - 20);
+      }
+      x += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+  return x;
+};
+
+
+
+
+
+
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -37,23 +64,7 @@ function statement(invoice, plays) {
     const play = plays[perf.playID];
     let thisAmount = 0;
 
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    thisAmount = calculateAmountBasedonPlayGenreAndAudience(play, perf);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
